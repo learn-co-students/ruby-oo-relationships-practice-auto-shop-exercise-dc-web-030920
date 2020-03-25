015baff7b94359be7714883ca7a76a8ea7808a55
rasdinho <ras.dinho7@gmail.com>
2020-03-25 15:23:01 -0400

    done with this

--- app/models/car.rb
+++ app/models/car.rb
@@ -1,11 +1,50 @@
+# class Car
+
+#   attr_reader :make, :model
+
+#   @@all = []
+
+#   def initialize(make, model, classification)
+#     @make = make
+#     @model = model
+#     @classification = classification
+#     @@all << self
+#   end
+
+#   def self.all
+#     @@all
+#   end
+# end
+# =====================================================
 class Car
+  attr_reader :classification, :owner, :mechanic
+  @@all = []
 
-  attr_reader :make, :model
-
-  def initialize(make, model, classification)
+  def initialize(owner, mechanic, make, model, classification)
+    @owner = owner
+    @mechanic = mechanic
     @make = make
     @model = model
     @classification = classification
+    @@all << self
   end
+
+  def self.all 
+    @@all
+  end 
+
+  def self.classifications
+    Car.all.map do |car|
+      car.classification
+    end 
+  end 
+
+  def self.find_mechanic(classification)
+    Mechanic.all.select do |mechanic|
+      mechanic.specialty == classification
+    end 
+    #select will always return SOME array 
+    #find will sometimes return nil
+  end 
 
 end
--- app/models/mechanic.rb
+++ app/models/mechanic.rb
@@ -1,11 +1,37 @@
+# 
+# =====================================================
 class Mechanic
+  attr_reader :name
+  attr_accessor :specialty
+  @@all = []
 
-  attr_reader :name, :specialty
+  def initialize(name, specialty)
+    @name = name
+    @specialty = specialty
+    @@all << self
+  end
 
-def initialize(name, specialty)
-  @name = name
-  @specialty = specialty
-end
+  def self.all 
+    @@all
+  end 
 
+  def cars 
+    Car.all.select do |car|
+      car.mechanic == self
+    end 
+  end 
+
+  def car_owners 
+    self.cars.map do |car|
+      car.owner
+    end 
+  end 
+
+  #type, name, logic
+  def car_owners_names
+    self.car_owners.map do |owner|   
+      owner.name    
+    end
+  end 
 
 end
